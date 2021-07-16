33ed204e905dfc65997948571533fc7b205ae837
Junwei Dai <junwei.dai@JunweideMacBook-Pro.local>
2021-07-16 15:04:29 +0800

    [junwei] run bookController with errors and didint know why

--- src/test/kotlin/demoInKotlin/BookControllerTest.kt
+++ src/test/kotlin/demoInKotlin/BookControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import java.time.LocalDateTime
 import kotlin.test.assertEquals
 
@@ -20,25 +21,31 @@ class BookControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-
     @Test
     fun `should get all book messages when get books`() {
+        var mockService = mockk<BookService>()
+        var bookController = BookController(mockService)
+        mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+
         var mockBooks = listOf<Books>(
             Books("1", "weiwei", "d", LocalDateTime.MAX),
             Books("2", "weiwei", "d", LocalDateTime.MIN)
         )
-//        var mockService = mockk<BookService>()
-//        var bookController = BookController(mockService)
-//        every { mockService.findBooks() } returns mockBooks
-//        bookController.books();
-//        assertEquals(2, bookController.books().size)
-        mockMvc.perform(MockMvcRequestBuilders
-            .get("/books")
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(String())
+        every { mockService.findBooks() } returns mockBooks
+
+
+
+        mockMvc.perform(
+            MockMvcRequestBuilders
+                .get("/books")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String())
         ).andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.content().string("[{\"id\":\"12\",\"name\":\"Hello!1111\",\"author\":\"Hello!1111\",\"writeAt\":\"2017-03-08T12:30:54\"},{\"id\":\"1\",\"name\":\"Hello!1111\",\"author\":\"Hello!1111\",\"writeAt\":\"2017-03-08T12:30:54\"}]"))
+            .andExpect(
+                MockMvcResultMatchers.content()
+                    .string("[{\"id\":\"12\",\"name\":\"Hello!1111\",\"author\":\"Hello!1111\",\"writeAt\":\"2017-03-08T12:30:54\"},{\"id\":\"1\",\"name\":\"Hello!1111\",\"author\":\"Hello!1111\",\"writeAt\":\"2017-03-08T12:30:54\"}]")
+            )
 
 
     }
